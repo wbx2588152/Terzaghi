48ea8939d06de8d188af711456b377a9e24b720c
wbx2588152 <wbx2588152@126.com>
2018-10-15 20:09:06 +0800

    覆盖提交

--- springcloud-feign/src/main/java/com/jk/service/Userservice.java
+++ springcloud-feign/src/main/java/com/jk/service/Userservice.java
@@ -1,8 +1,9 @@
 package com.jk.service;
 
+import com.jk.service.impl.UserError;
 import org.springframework.cloud.openfeign.FeignClient;
 
-@FeignClient(value="service-ha")
+@FeignClient(value="service-ha",fallback = UserError.class)
 public interface Userservice extends UserServiceApi {
 
 }
---
+++ springcloud-feign/src/main/java/com/jk/service/impl/UserError.java
@@ -0,0 +1,43 @@
+package com.jk.service.impl;
+
+import com.jk.model.User;
+
+
+import com.jk.service.Userservice;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+@Component
+public class UserError implements Userservice {
+    @Override
+    public List<User> sayHafromOneClient() {
+        List<User> list=new ArrayList<>();
+        User user =new User();
+        user.setMycode("sorry,error!");
+        list.add(user);
+        return list;
+    }
+
+    @Override
+    public void saveWbxUser(User user) {
+
+    }
+
+    @Override
+    public void deleteWbxUser(String id) {
+
+    }
+
+    @Override
+    public User queryWbxUserById(String id) {
+        User user =new User();
+        user.setMycode("sorry,error!");
+        return user;
+    }
+
+    @Override
+    public void updateWbxUser(User user) {
+
+    }
+}
