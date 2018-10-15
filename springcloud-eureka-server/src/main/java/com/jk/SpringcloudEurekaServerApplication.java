b454c217d4691097370efee7c80a1737f1226280
wbx2588152 <wbx2588152@126.com>
2018-10-15 13:39:00 +0800

    三

--- springcloud-eureka-server/src/main/java/com/jk/SpringcloudEurekaServerApplication.java
+++ springcloud-eureka-server/src/main/java/com/jk/SpringcloudEurekaServerApplication.java
@@ -7,8 +7,6 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 @EnableEurekaServer
 public class SpringcloudEurekaServerApplication {
-
-
     public static void main(String[] args) {
         SpringApplication.run(SpringcloudEurekaServerApplication.class, args);
     }
