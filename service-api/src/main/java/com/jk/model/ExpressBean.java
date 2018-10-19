79a463efcb70fd6312d39b8bf0df7a84c6c3c493
xulisa <2392457144@qq.com>
2018-10-19 22:36:00 +0800

    物流管理表

---
+++ service-api/src/main/java/com/jk/model/ExpressBean.java
@@ -0,0 +1,49 @@
+package com.jk.model;
+
+import java.io.Serializable;
+
+/**
+ * 物流管理表
+ */
+public class ExpressBean implements Serializable {
+
+    private String expressid;// 主键id
+
+    private String expresscompany; // 物流公司
+
+    private String experssnumber; // 物流单号
+
+    private String linkorderid; // 关联订单号
+
+    public String getExpressid() {
+        return expressid;
+    }
+
+    public void setExpressid(String expressid) {
+        this.expressid = expressid;
+    }
+
+    public String getExpresscompany() {
+        return expresscompany;
+    }
+
+    public void setExpresscompany(String expresscompany) {
+        this.expresscompany = expresscompany == null ? null : expresscompany.trim();
+    }
+
+    public String getExperssnumber() {
+        return experssnumber;
+    }
+
+    public void setExperssnumber(String experssnumber) {
+        this.experssnumber = experssnumber == null ? null : experssnumber.trim();
+    }
+
+    public String getLinkorderid() {
+        return linkorderid;
+    }
+
+    public void setLinkorderid(String linkorderid) {
+        this.linkorderid = linkorderid == null ? null : linkorderid.trim();
+    }
+}
