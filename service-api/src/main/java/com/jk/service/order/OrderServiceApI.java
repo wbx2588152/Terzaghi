92fca63d1b0d9cf9567c2b1f6a1c7ab3a4dbc4ca
xulisa <2392457144@qq.com>
2018-10-26 11:49:16 +0800

    service Api 第二次上传;

--- service-api/src/main/java/com/jk/service/order/OrderServiceApI.java
+++ service-api/src/main/java/com/jk/service/order/OrderServiceApI.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
 import java.util.Map;
 
 public interface OrderServiceApI {
@@ -22,12 +23,16 @@ public interface OrderServiceApI {
     @RequestMapping(value = "oneclient/generateOrdeBean",method = RequestMethod.POST)
     void generateOrdeBean(@RequestBody OrderBean orderBean);
 
-
-
     @RequestMapping(value = "oneclient/queryorderalldata",method = RequestMethod.POST)
     Map<String, Object> queryorderalldata(@RequestParam(value = "orderid") String orderid);
 
+    @RequestMapping(value = "oneclient/savedeliverdata",method = RequestMethod.POST)
+    void savedeliverdata(@RequestParam(value = "consignnum") String consignnum, @RequestParam(value = "expresscompany") String expresscompany, @RequestParam(value = "orderid") String orderid);
+
+    @RequestMapping(value = "oneclient/initselone",method = RequestMethod.POST)
+    List<OrderBean> initselone();
 
 
-
+    @RequestMapping(value = "oneclient/mergerorders",method = RequestMethod.POST)
+    void mergerorders(@RequestParam(value = "mainorder") String mainorder, @RequestParam("subarr") String subarr);
 }
