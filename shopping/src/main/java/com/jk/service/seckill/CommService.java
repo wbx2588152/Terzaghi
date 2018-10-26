29fb76cb0ee7fc8aec89e7387365125655084b4f
wbx2588152 <wbx2588152@126.com>
2018-10-26 08:57:43 +0800

    商品列表 购物车 一些小改动

--- shopping/src/main/java/com/jk/service/seckill/CommService.java
+++ shopping/src/main/java/com/jk/service/seckill/CommService.java
@@ -3,6 +3,6 @@ package com.jk.service.seckill;
 import com.jk.service.CommServiceApi;
 import org.springframework.cloud.openfeign.FeignClient;
 
-@FeignClient(value="service-ha")
+@FeignClient(value="service-ha2")
 public interface CommService extends CommServiceApi {
 }
